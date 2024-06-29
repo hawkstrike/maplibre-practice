e6033792768b287e8522670a0fc40be4dd363f93
fedaykin <fremanfedaykin@gmail.com>
2024-06-29 17:11:19 +0900

    feat: 지도 기본 틀 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,60 @@
-import { Map, MapProvider } from 'react-map-gl';
+import MapboxLanguage from '@mapbox/mapbox-gl-language';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { Map, MapProvider, MapWheelEvent, Marker, NavigationControl } from 'react-map-gl';
 import './App.css';
+
+const accessToken: string = (import.meta.env.VITE_MAP_BOX_ACCESS_TOKEN || '').trim();
 
 function App() {
 
   return (
     <MapProvider>
       <Map
-        accessToken=''
+        mapboxAccessToken={accessToken}
         id='maplibre-practice'
         initialViewState={{
-          latitude: 37.7749,
-          longitude: 122.4194,
-          zoom: 11
+          longitude: 126.9170971,
+          latitude: 37.3981977,
+          zoom: 16.3
         }}
         onLoad={(event) => {
-          event.target.resize();
+          console.log('event onLoad :', event);
+          // event.target.resize();
         }}
-      />
+        onStyleData={(event) => {
+          console.log('event onStyleData :', event);
+          event.target.addControl(new MapboxLanguage({ defaultLanguage: 'ko' }));
+          // event.target.resize();
+        }}
+        onRender={(event) => {
+          // console.log('event onRender :', event);
+        }}
+        onMove={(event) => {
+          // console.log('event onMove :', event);
+        }}
+        onWheel={(event: MapWheelEvent) => {
+          console.log('event onWheel :', event);
+        }}
+        onIdle={(event) => {
+          console.log('event onIdle :', event);
+        }}
+        doubleClickZoom={false}
+        dragRotate={false}
+        attributionControl={false}
+        style={{
+          width: '1200px',
+          height: '1000px'
+        }}
+        mapStyle="mapbox://styles/mapbox/navigation-day-v1"
+      >
+        <NavigationControl
+          showCompass={true}
+          showZoom={true}
+          position='bottom-right'
+        />
+        <Marker longitude={126.9170971} latitude={37.3981977} anchor="bottom">
+        </Marker>
+      </Map>
     </MapProvider>
   );
 }
