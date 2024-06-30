6c05a9114b9e9aa9ba047738e9a705f502a483c9
fedaykin <fremanfedaykin@gmail.com>
2024-06-30 21:23:26 +0900

    feat: vite-plugin-svgr 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [react()],
+  /* build: {
+    assetsDir: 'static',
+    copyPublicDir: true,
+  }, */
+  publicDir: 'src/assets',
+  plugins: [react(), svgr()],
 });
